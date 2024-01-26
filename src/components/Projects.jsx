5473005955d7da15b0f09ba9f9fece6725672180
Jesmi-04 <jesmigeorge04@gmail.com>
2024-01-27 00:50:08 +0530

    added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import ProjectItem from './ProjectItem'
 import news from '../assets/news.png'
 import leetcode from '../assets/leetcode.png'
 import starbucks from '../assets/starbucks.png'
+import eats from '../assets/eat.png'
 export default function Projects() {
   return (
     <div id='projects'>
@@ -10,7 +11,8 @@ export default function Projects() {
             <h1 className='section-header my-9'>Projects</h1>
             <div className='grid sm:grid-cols-2 gap-12'>
                 <ProjectItem img={news} title='News App' url='/'/>
-                <ProjectItem img={leetcode} title='LeetCode Recommendation' url='https://drive.google.com/file/d/1UBhbx_A_I9FSX-2bWyHUpN2PK4Ghiie7/view?usp=drive_link' />
+                <ProjectItem img={eats} title='Best Eats App' url='https://best-eats-hotel.netlify.app/'/>
+                <ProjectItem img={leetcode} title='LeetCode Recommendation' url='https://github.com/jesmigeorge/Leetcode-Recommendation' />
                 <ProjectItem img={starbucks} title='StarBucks Ui Clone' url='https://jesmigeorge.github.io/Starbucks-Clone/'/>
             </div>
         </div>
