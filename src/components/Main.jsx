bb77245fa2fcd8df365d77214f318fe1d7877fe0
Jesmi-04 <jesmigeorge04@gmail.com>
2024-01-27 20:06:02 +0530

    resume updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import bgimg from "../assets/bgimg.jpg";
 import { TbBrandLeetcode } from "react-icons/tb";
 export default function Main() {
     const handleResume = ()=>{
-        window.open("https://drive.google.com/file/d/1_K8NfMIF03i0ellhK2_8-YARxTNDEmLG/view?usp=sharing", '_blank')
+        window.open("https://drive.google.com/file/d/1MAiL_Or-v9jMAAIHHYC-BankWKbCIhQB/view?usp=sharing", '_blank')
     }
   return (
     <div id='main'>
